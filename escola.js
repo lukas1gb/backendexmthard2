38dcb7904b9423c8ca0e7a0ad11c20cae10e376f
lukas1gb <lukas.prestes@escola.pr.gov.br>
2026-02-27 11:41:17 -0300

    Add files via upload

---
+++ escola.js
@@ -0,0 +1,13 @@
+const alunos = [
+    { nome: "Ana", nota: 8.5, idade: 16, turma: "3A" },
+    { nome: "João", nota: 6.0, idade: 17, turma: "3B" },
+    { nome: "Maria", nota: 9.0, idade: 16, turma: "3A" },
+    { nome: "Pedro", nota: 4.5, idade: 17, turma: "3B" },
+    { nome: "Julia", nota: 7.5, idade: 16, turma: "3A" },
+    { nome: "Carlos", nota: 5.5, idade: 17, turma: "3B" }
+  ];
+  filter (nota>=6)
+  alunos.filter(aluno => aluno.nota >= 6);
+  console.log("Alunos aprovados:");
+  console.log(aprovados);
+  
